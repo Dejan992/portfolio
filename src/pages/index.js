c4a4a2ffb25a789dc0e10dd03c48f3a35ce7d65c
Dejan Rajkovic <drajkovic@Dejans-MacBook-Pro.local>
2020-03-30 11:11:58 -0600

    Change of address

--- src/pages/index.js
+++ src/pages/index.js
@@ -67,12 +67,12 @@ class HomeIndex extends React.Component {
 
                     <section id="one">
                         <header className="major">
-                            <h2>As a detail-oriented developer I strive to build immersive and beautiful web applications through carefully crafted code and user-centric design.
+                            <h2>AWS Certified Solutions Architect with 2 years of technical experience in CI/CD pipelines, monitoring, and extensive understanding of high availability AWS cloud architecture and concepts.
                             </h2>
                         </header>
         
-                        <p>Software development is something I am passionate about, it gives me ability to express myself creatively in a nontraditional way. I love tackling difficult problems and participating in challenging endeavors which make a difference in the world. As a graduate of Fullstack Academy I look forward to continue learning and immersing myself in the field of web development.</p>
-                        <p>When not online, I can be found volunteering at Alive Rescue in Chicago. <br/> If you love dogs, consider donating <strong><a href='http://www.aliverescue.org/'>http://www.aliverescue.org/</a></strong>  <br/>Let's save the world one tail at the time</p>
+                        <p>DevOps is something I am passionate about, it gives me ability to express myself creatively in a nontraditional way. I love tackling difficult problems and participating in challenging endeavors which make a difference in the world.</p>
+                        <p>When not online, I can be found hiking or snowboarding on Colorado mountains, I am still in an early phase of my snowboarding journey so no black diamonds yet. <br/> If you love dogs as much as I do, consider donating <strong><a href='http://www.aliverescue.org/'>http://www.aliverescue.org/</a></strong>  <br/>Let's save the world one tail at the time</p>
                         <ul className="actions">
                             {/* <li><a href="#" className="button">Learn More</a></li> */}
                         </ul>
@@ -100,8 +100,8 @@ class HomeIndex extends React.Component {
                                 <ul className="labeled-icons">
                                     <li>
                                         <h3 className="icon fa-home"><span className="label">Address</span></h3>
-                                        2025 W Roscoe St. 3r<br />
-                                        Chicago, IL 60618<br />
+                                        2424 W Caithness Pl<br />
+                                        Denver, CO 80211<br />
                                         United States
                                     </li>
                                     <li>
